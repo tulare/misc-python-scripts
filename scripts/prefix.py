c4081dc55336a80e6532adcea0dd267a711d2af0
tulare <tulare.paxgalactica@gmail.com>
2019-07-09 14:20:26 +0200

    Fix prefix.py

--- scripts/prefix.py
+++ scripts/prefix.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 import argparse
+import logging
 from pathlib import Path
 
+# ------------------------------------------------------------------------------
 
 def parse_args() :
     parser = argparse.ArgumentParser()
@@ -15,14 +17,6 @@ def parse_args() :
     )
     parser.add_argument(
         '-Z', '--add-after',
-        default=''
-    )
-    parser.add_argument(
-        '-H', '--remove-head',
-        default=''
-    )
-    parser.add_argument(
-        '-T', '--remove-tail',
         default=''
     )
     parser.add_argument(
@@ -42,62 +36,56 @@ def parse_args() :
         default=''
     )
     parser.add_argument(
-        '-C', '--mod-from',
+        '--mod-from',
         default=''
     )
     parser.add_argument(
-        '-E', '--to-end',
+        '--to',
         default=''
     )    
     parser.add_argument(
-        '-Y', '--by',
+        '--by',
         default=''
     )
     args = parser.parse_args()
-    print(args)
+    logging.debug(args)
     return args
+
+# ------------------------------------------------------------------------------
 
 def add_before(file, text='') :
     new_name = text + file.name
     file.rename(new_name)
-    print(new_name)
+    return new_name
+
+# ------------------------------------------------------------------------------
 
 def add_after(file, text='') :
     new_name = file.stem + text + file.suffix
     file.rename(new_name)
-    print(new_name)
+    return new_name
 
-def remove_tail(file, here='') :
-    new_name = file.name
-    parts = file.stem.rpartition(here)
-    if parts[1] == here and parts[2] == '' :
-        new_name = parts[0] + file.suffix
-        file.rename(new_name)
-    print(new_name)
-
-def remove_head(file, here='') :
-    new_name = file.name
-    parts = file.stem.partition(here)
-    if parts[1] == here and parts[0] == '' :
-        new_name = parts[2] + file.suffix
-        file.rename(new_name)
-    print(new_name)
+# ------------------------------------------------------------------------------
 
 def cut_before(file, here='') :
     new_name = file.name
     parts = file.stem.partition(here)
-    if parts[1] == here and parts[0] != '' :
+    if parts[1] == here :
         new_name = parts[2] + file.suffix
         file.rename(new_name)
-    print(new_name)
+    return new_name
+
+# ------------------------------------------------------------------------------
 
 def cut_after(file, here='') :
     new_name = file.name
-    parts = file.stem.partition(here)
-    if parts[1] == here and parts[2] != '' :
+    parts = file.stem.rpartition(here)
+    if parts[1] == here :
         new_name = parts[0] + file.suffix
         file.rename(new_name)
-    print(new_name)
+    return new_name
+
+# ------------------------------------------------------------------------------
 
 def mod_left(file, here='', by='') :
     new_name = file.name
@@ -105,7 +93,9 @@ def mod_left(file, here='', by='') :
     if parts[1] == here :
         new_name = parts[0] + by + parts[2] + file.suffix
         file.rename(new_name)
-    print(new_name)
+    return new_name
+
+# ------------------------------------------------------------------------------
 
 def mod_right(file, here='', by='') :
     new_name = file.name
@@ -113,42 +103,45 @@ def mod_right(file, here='', by='') :
     if parts[1] == here :
         new_name = parts[0] + by + parts[2] + file.suffix
         file.rename(new_name)
-    print(new_name)
+    return new_name
+
+# ------------------------------------------------------------------------------
 
 def mod_center(file, start='', end='', by='') :
     new_name = file.name
     sparts = file.stem.partition(start)
     if sparts[1] == start :
-        eparts = sparts[2].partition(end)
+        eparts = sparts[2].rpartition(end)
         if eparts[1] == end :
             new_name = sparts[0] + by + eparts[2] + file.suffix
             file.rename(new_name)
-    print(new_name)
-        
+    return new_name
+
+# ------------------------------------------------------------------------------
 
 if __name__ == '__main__' :
+
+    logging.basicConfig(level=logging.INFO)
 
     args = parse_args()
 
     p = Path('.')
     for file in (x for x in p.glob(args.pattern) if x.is_file()) :
-        print(file, end=' -> ')
+        new_name = ''
         if args.add_before != '' :
-            add_before(file, args.add_before)
+            new_name = add_before(file, args.add_before)
         if args.add_after != '' :
-            add_after(file, args.add_after)
-        if args.remove_head != '' :
-            remove_head(file, args.remove_head)
-        if args.remove_tail != '' :
-            remove_tail(file, args.remove_tail)
+            new_name = add_after(file, args.add_after)
         if args.cut_before != '' :
-            cut_before(file, args.cut_before)
+            new_name = cut_before(file, args.cut_before)
         if args.cut_after != '' :
-            cut_after(file, args.cut_after)
+            new_name = cut_after(file, args.cut_after)
         if args.mod_left != '' :
-            mod_left(file, args.mod_left, args.by)
+            new_name = mod_left(file, args.mod_left, args.by)
         if args.mod_right != '' :
-            mod_right(file, args.mod_right, args.by)
-        if args.mod_from != '' and args.to_end != '' :
-            mod_center(file, start=args.mod_from, end=args.to_end, by=args.by)
+            new_name = mod_right(file, args.mod_right, args.by)
+        if args.mod_from != '' and args.to != '' :
+            new_name = mod_center(file, start=args.mod_from, end=args.to, by=args.by)
+        operation = 'FOUND' if new_name == '' else 'RENAME'
+        logging.info('[{}] {} {}'.format(operation, file, new_name))
         
